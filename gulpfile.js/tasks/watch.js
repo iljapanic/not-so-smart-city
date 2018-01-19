c54504724b72e5209253997a61453e5a55d96e3a
Ilja <ilja@iljapanic.me>
2018-01-19 16:43:20 +0000

    mobile and tablet adjustments

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -4,7 +4,7 @@ const watch = require('gulp-watch');
 
 const watchTask = function() {
 	watch([config.markdown.watch, config.markdown.watchIgnore], function() { gulp.start('markdown'); });
-	watch(config.html.watch, function() { gulp.start('html'); });
+	watch([config.html.watch, config.html.watchIgnore], function() { gulp.start('html'); });
 	watch(config.data.watch, function() { gulp.start('html'); });
 	watch(config.css.watch, function() { gulp.start('css'); });
 	watch(config.js.watch, function() { gulp.start('js'); });
